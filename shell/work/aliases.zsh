6000f3d78af19602dfbb589cbb8b173ec08937c9
Ting Zhou <ztlevi.work@gmail.com>
2020-08-08 10:10:21 -0700

    shell/work: update source condition

--- shell/work/aliases.zsh
+++ shell/work/aliases.zsh
@@ -1,1 +1,3 @@
-source $HOME/.work/dots/aliases.zsh
+if [ -f $HOME/.work/dots/aliases.zsh ]; then
+  source $HOME/.work/dots/aliases.zsh
+fi
--- shell/work/env.zsh
+++ shell/work/env.zsh
@@ -1,1 +1,3 @@
-source $HOME/.work/dots/env.zsh
+if [ -f $HOME/.work/dots/env.zsh ]; then
+  source $HOME/.work/dots/env.zsh
+fi
