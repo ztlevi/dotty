1562217c23897a096af697988737ec59f9b3d27c
Ting Zhou <ztlevi.work@gmail.com>
2020-09-10 09:57:21 -0700

    dev/python: add . back to PYTHONPATH

--- dev/python/env.zsh
+++ dev/python/env.zsh
@@ -3,7 +3,9 @@ export PIP_LOG_FILE="$XDG_DATA_HOME/pip/log"
 export PYLINTHOME="$XDG_DATA_HOME/pylint"
 export PYLINTRC="$XDG_CONFIG_HOME/pylint/pylintrc"
 export IPYTHONDIR="$XDG_CONFIG_HOME/ipython"
-# Fro python3.7 and higher
+
+export PYTHONPATH=.:$PYTHONPATH
+# For python3.7 and higher
 # Set breakpoint() in Python to call pudb
 export PYTHONBREAKPOINT="pudb.set_trace"
 
