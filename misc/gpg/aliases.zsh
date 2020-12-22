6c8789716be0140cfdc6f38c85d53985b92ac52e
Ting Zhou <ztlevi.work@gmail.com>
2020-12-21 18:55:51 -0800

    misc/gpg: update

--- misc/gpg/aliases.zsh
+++ misc/gpg/aliases.zsh
@@ -1,2 +1,6 @@
+if ! _is_running gpg-agent; then
+  mkdir -p $HOME/.gnupg
+  _run gpg-agent --homedir $HOME/.gnupg --use-standard-socket --daemon >/dev/null
+fi
 export GPG_TTY="$(tty)"
 _run gpg-connect-agent updatestartuptty /bye >/dev/null
