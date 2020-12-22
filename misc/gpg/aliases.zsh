72cbeff1ae17d34c87ec4278393b24e8da58fad9
Ting Zhou <ztlevi.work@gmail.com>
2020-12-22 00:00:01 -0800

    misc/gpg: fallback to curses when in ssh

--- misc/gpg/aliases.zsh
+++ misc/gpg/aliases.zsh
@@ -1,6 +1,5 @@
-# if ! _is_running gpg-agent; then
-#   mkdir -p $HOME/.gnupg
-#   _run gpg-agent --homedir $HOME/.gnupg --use-standard-socket --daemon >/dev/null
-# fi
-# export GPG_TTY="$(tty)"
-# _run gpg-connect-agent updatestartuptty /bye >/dev/null
+# Fallback to pinentry-curses if in ssh terminal
+if [[ -n "$SSH_CONNECTION" ]]; then
+  export GPG_TTY="$(tty)"
+  export PINENTRY_USER_DATA="USER_CURSES=1"
+fi
