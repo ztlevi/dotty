530f96983dd03ea2b38d56291901ead18d93676a
Ting Zhou <ztlevi.work@gmail.com>
2020-12-25 10:29:02 -0800

    shell/git: update

--- misc/gpg/aliases.zsh
+++ misc/gpg/aliases.zsh
@@ -3,13 +3,3 @@ export GPG_TTY="$(tty)"
 if [[ -n "$SSH_CONNECTION" ]]; then
   export PINENTRY_USER_DATA="USER_CURSES=1"
 fi
-
-if [[ -n "$SSH_CONNECTION" ]]; then
-  git config --global --unset user.signingkey
-  git config --global --unset commit.gpgsign
-  git config --global --unset tag.gpgsign
-else
-  git config --global --add user.signingkey ztlevi.work@gmail.com
-  git config --global --add commit.gpgsign true
-  git config --global --add tag.gpgsign true
-fi
--- shell/git/aliases.zsh
+++ shell/git/aliases.zsh
@@ -2,3 +2,14 @@ alias git='noglob git'
 alias g="git"
 alias gci="gh pr checks"
 alias gcpr="gh pr create --assignee ztlevi"
+
+local SINGING_KEY=$(git config --global --get user.signingkey)
+if [[ -n "$SSH_CONNECTION" ]]; then
+  git config --global --unset user.signingkey
+  git config --global --unset commit.gpgsign
+  git config --global --unset tag.gpgsign
+elif [[ -z "$SINGING_KEY" ]]; then
+  git config --global --add user.signingkey ztlevi.work@gmail.com
+  git config --global --add commit.gpgsign true
+  git config --global --add tag.gpgsign true
+fi
