836090f554902fc6ff5d67d817a5eeb5db13c04c
Ting Zhou <ztlevi.work@gmail.com>
2021-02-05 12:39:40 +0000

    dev/node: BUMP node version

--- dev/node/env.zsh
+++ dev/node/env.zsh
@@ -2,7 +2,7 @@ export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
 export NODE_REPL_HISTORY=$XDG_CACHE_HOME/node/repl_history
 
 export NODENV_ROOT=$XDG_DATA_HOME/nodenv
-export NODE_VERSION=12.10.0
+export NODE_VERSION=14.15.4
 export NPM_GLOBAL_DIR=$XDG_DATA_HOME/nodenv/versions/$NODE_VERSION
 
 path=($NODENV_ROOT/bin $NPM_GLOBAL_DIR/bin $path)
