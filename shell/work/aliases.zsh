31b06774c3dcce41586cf37f62412fbbd410e8fa
Ting Zhou <ztlevi.work@gmail.com>
2021-02-20 12:05:45 +0800

    shell/work: update

--- shell/work/aliases.zsh
+++ shell/work/aliases.zsh
@@ -1,3 +1,5 @@
-if [ -f $HOME/.work/dots/aliases.zsh ]; then
+if [[ -f $HOME/workplace/work/dots/aliases.zsh ]]; then
+  source $HOME/workplace/work/dots/aliases.zsh
+elif [[ -f $HOME/.work/dots/aliases.zsh ]]; then
   source $HOME/.work/dots/aliases.zsh
 fi
--- shell/work/env.zsh
+++ shell/work/env.zsh
@@ -1,3 +1,5 @@
-if [ -f $HOME/.work/dots/env.zsh ]; then
+if [[ -f $HOME/workplace/work/dots/env.zsh ]]; then
+  source $HOME/workplace/work/dots/env.zsh
+elif [ -f $HOME/.work/dots/env.zsh ]; then
   source $HOME/.work/dots/env.zsh
 fi
