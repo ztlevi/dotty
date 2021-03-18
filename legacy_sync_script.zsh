ff20b97d086ea30af384918147c9e1022b0d2f2d
Ting Zhou <ztlevi.work@gmail.com>
2021-03-18 22:17:55 +0800

    misc/chinese: emacs use different rime folder

--- legacy_sync_script.zsh
+++ legacy_sync_script.zsh
@@ -1,8 +1,3 @@
 #!/usr/bin/env zsh
 # This script is used in `shell/zsh/utils.zsh` update_my_repos. Whenver you have something you want to sync
 # across machines, put the scritps here.
-
-# brew uninstall git-standup
-# brew link git-extras
-
-rm -f $HOME/Library/DefaultKeyBinding.dict
