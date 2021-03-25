4066f276cf7c76b6d497a2daf8bcdd34a17e8e5c
Ting Zhou <ztlevi.work@gmail.com>
2021-03-25 18:09:40 +0800

    base/macos: move script

---
+++ base/macos/scripts/set-rectangle-defaults.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Rectangle setup, see ~/Library/Preferences/com.knollsoft.Rectangle.plist
+defaults write com.knollsoft.Rectangle leftHalf '{keyCode = 4; modifierFlags = 1310720;}'
+defaults write com.knollsoft.Rectangle rightHalf '{keyCode = 37; modifierFlags = 1310720;}'
+defaults write com.knollsoft.Rectangle maximize '{keyCode = 46; modifierFlags = 1310720;}'
+defaults write com.knollsoft.Rectangle almostMaximize '{keyCode = 45; modifierFlags = 1310720;}'
+defaults write com.knollsoft.Rectangle bottomHalf '{keyCode = 38; modifierFlags = 1310720;}'
+defaults write com.knollsoft.Rectangle topHalf '{keyCode = 40; modifierFlags = 1310720;}'
+defaults write com.knollsoft.Rectangle launchOnLogin -bool true
+defaults write com.knollsoft.Rectangle hideMenubarIcon -bool true
+defaults write com.knollsoft.Rectangle windowSnapping -int 2
+defaults write com.knollsoft.Rectangl subsequentExecutionMode -int 1
