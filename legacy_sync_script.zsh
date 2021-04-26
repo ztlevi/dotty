558e5893d5214ab2161517ae59a07a91b987cfca
Ting Zhou <ztlevi.work@gmail.com>
2021-04-26 20:29:11 +0800

    dev: use manual rehash

--- legacy_sync_script.zsh
+++ legacy_sync_script.zsh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
 # This script is used in `shell/zsh/utils.zsh` update_my_repos. Whenver you have something you want to sync
 # across machines, put the scritps here.
+
+_is_callable nodenv && rm -rf "$(nodenv root)"/plugins/nodenv-package-rehash
