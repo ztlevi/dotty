0aaa0095aa536c897f948d34dcce54a484915320
Ting Zhou <ztlevi.work@gmail.com>
2021-06-02 23:26:35 +0800

    shell/iterm: update config

--- misc/apps/aliases.zsh
+++ misc/apps/aliases.zsh
@@ -2,6 +2,7 @@ _is_callable tensorboard && alias tb="tensorboard"
 _is_callable bandwhich && alias bandwhich="sudo $(which bandwhich)"
 _is_callable code && alias c.="code ."
 _is_callable cspell && alias cspell-all="cspell --config '$HOME/misc/apps/.cspell.json' '**/*.{js,jsx,ts,tsx,c,cc,cpp,h,hh,hpp,go,json,py}'"
+_is_callable glances && alias glances="glances --theme-white"
 
 # used for mas
 function find-app-id() {
