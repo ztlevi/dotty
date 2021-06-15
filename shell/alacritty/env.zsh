5e57a14ac77b3d5138a69208969467b6ebbd52f9
Ting Zhou <ztlevi.work@gmail.com>
2021-06-15 17:36:43 +0800

    shell/alacritty: update vivid env

--- shell/alacritty/env.zsh
+++ shell/alacritty/env.zsh
@@ -1,4 +1,4 @@
 # https://github.com/sharkdp/vivid/tree/master/themes
 # Genereate with `vivid generate molokai`
 # for terminal not support use `--color-mode 8-bit`
-export LS_COLORS="$(vivid generate solarized-light)"
+export LS_COLORS="$(vivid generate one-light)"
