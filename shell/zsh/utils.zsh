734b527e4d75c01a0720aac0bd0518626e9cb6e9
Ting Zhou <ztlevi.work@gmail.com>
2021-07-03 18:45:00 +0800

    shell/zsh: utility add palette

--- shell/zsh/utils.zsh
+++ shell/zsh/utils.zsh
@@ -4,6 +4,15 @@ function string_join() {
   echo "$*"
 }
 
+function palette() {
+  for i in {0..255} ; do
+    printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
+    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
+      printf "\n";
+    fi
+  done
+}
+
 center_text() {
   # Function "center_text": center the text with a surrounding border
 
