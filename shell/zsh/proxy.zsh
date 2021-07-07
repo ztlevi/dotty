f11cf2e9c06392c5d11756085e4f9127d9d647c8
Ting Zhou <ztlevi.work@gmail.com>
2021-07-07 12:49:04 +0800

    shell/zsh: update proxy

--- shell/zsh/proxy.zsh
+++ shell/zsh/proxy.zsh
@@ -1,13 +1,18 @@
 #!/usr/bin/env zsh
 
 # ClashX Pro: Copy shell command
-http_port=7890
-socks_port=7890
-proxyon() {
+clash_proxyon() {
+  http_port=7890
+  socks_port=7890
   export http_proxy="http://127.0.0.1:$http_port"
   export https_proxy="http://127.0.0.1:$http_port"
   export all_proxy="socks5://127.0.0.1:$socks_port"
-  echo "Proxy on"
+  echo "Clash proxy on"
+}
+ss_proxyon() {
+  export http_proxy=socks5://127.0.0.1:1080
+  export https_proxy=socks5://127.0.0.1:1080
+  echo "ShadowSockets proxy on"
 }
 proxyoff() {
   unset http_proxy
