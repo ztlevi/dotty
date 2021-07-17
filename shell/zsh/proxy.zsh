c172d35e6569aed14cf229c72290c407f668cd9c
Ting Zhou <ztlevi.work@gmail.com>
2021-07-17 09:19:29 +0800

    shell/utils: update proxy

--- shell/zsh/proxy.zsh
+++ shell/zsh/proxy.zsh
@@ -7,16 +7,16 @@ clash_proxyon() {
   export http_proxy="http://127.0.0.1:$http_port"
   export https_proxy="http://127.0.0.1:$http_port"
   export all_proxy="socks5://127.0.0.1:$socks_port"
-  echo "Clash proxy on"
+  echo ${fg_bold[white]}${bg[blue]}"Clash proxy on"${reset_color}
 }
 ss_proxyon() {
   export http_proxy=socks5://127.0.0.1:1080
   export https_proxy=socks5://127.0.0.1:1080
-  echo "ShadowSockets proxy on"
+  echo ${fg_bold[white]}${bg[blue]}"ShadowSockets proxy on"${reset_color}
 }
 proxyoff() {
   unset http_proxy
   unset https_proxy
   unset all_proxy
-  echo "Proxy off"
+  echo ${fg_bold[white]}${bg[blue]}"Proxy off"${reset_color}
 }
