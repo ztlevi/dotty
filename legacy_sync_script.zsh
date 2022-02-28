db28f002b7f00e9ce3ac420c4ec8292c1beaaf11
Ting Zhou <ztlevi.work@gmail.com>
2022-02-27 16:43:20 -0800

    shell/git: use git delta

--- legacy_sync_script.zsh
+++ legacy_sync_script.zsh
@@ -2,4 +2,5 @@
 # This script is used in `shell/zsh/utils.zsh` update_my_repos. Whenver you have something you want to sync
 # across machines, put the scritps here.
 
-brew install -f eva
+brew uninstall -f diff-so-fancy
+brew install -f git-delta
