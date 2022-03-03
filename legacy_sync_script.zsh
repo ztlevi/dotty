798dc816f927fbd36f4d5b21e903e2438fee2e01
Ting Zhou <ztlevi.work@gmail.com>
2022-03-02 16:03:26 -0800

    remove sync scripts

--- legacy_sync_script.zsh
+++ legacy_sync_script.zsh
@@ -2,11 +2,3 @@
 # This script is used in `shell/zsh/utils.zsh` update_my_repos. Whenver you have something you want to sync
 # across machines, put the scritps here.
 
-brew uninstall -f diff-so-fancy
-brew install -f git-delta
-
-brew uninstall -f fzf
-rm -f $HOME/.zinit/polaris/bin/fzf $HOME/.zinit/completions/_fzf_completion
-
-D -d shell/fzf
-D shell/skim
