5d2992504901362d61e8c5a441445926f09a0dd1
Ting Zhou <ztlevi.work@gmail.com>
2022-04-12 13:28:09 -0700

    dev/python: remove pyenv

--- legacy_sync_script.zsh
+++ legacy_sync_script.zsh
@@ -2,3 +2,4 @@
 # This script is used in `shell/zsh/utils.zsh` update_my_repos. Whenver you have something you want to sync
 # across machines, put the scritps here.
 
+rm -rf "$XDG_DATA_HOME/pyenv" $XDG_CACHE_HOME/${SHELL##*/}/pyenv
