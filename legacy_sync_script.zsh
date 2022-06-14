53d324143edf646d6733ccfb1bea015714ef9fad
Ting Zhou <ztlevi.work@gmail.com>
2022-06-14 01:56:14 -0700

    dev/java: update java installer

--- legacy_sync_script.zsh
+++ legacy_sync_script.zsh
@@ -3,3 +3,6 @@
 # across machines, put the scritps here.
 
 brew uninstall -f openssh
+
+brew uninstall -f adoptopenjdk/openjdk/adoptopenjdk8 adoptopenjdk/openjdk/adoptopenjdk11
+brew untap -f adoptopenjdk/openjdk
