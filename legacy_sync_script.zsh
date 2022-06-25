3e10706d1c9b6d26e69ba43c005dbbc6d7eb5416
Ting Zhou <ztlevi.work@gmail.com>
2022-06-25 15:10:29 -0700

    shell/zsh: use ds

--- legacy_sync_script.zsh
+++ legacy_sync_script.zsh
@@ -2,7 +2,12 @@
 # This script is used in `shell/zsh/utils.zsh` update_my_repos. Whenver you have something you want to sync
 # across machines, put the scritps here.
 
-brew uninstall -f openssh
-
+brew uninstall -f openssh zshdb bashdb dust
 brew uninstall -f adoptopenjdk/openjdk/adoptopenjdk8 adoptopenjdk/openjdk/adoptopenjdk11
 brew untap -f adoptopenjdk/openjdk
+
+if !_is_callable ds; then
+  # Deploy rust if not yet deployed, and install ds
+  ! topic-enabled-p dev/rust && dotty dev/rust
+  cargo install dirstat-rs
+fi
