5ed891db296b73fe38ddc648b6d248cf3641a4b4
Ting Zhou <ztlevi.work@gmail.com>
2024-02-21 13:21:33 -0800

    misc/sketchybar: update installation

--- base/macos/set-rectangle-defaults.sh
+++ base/macos/set-rectangle-defaults.sh
@@ -11,3 +11,5 @@ defaults write com.knollsoft.Rectangle launchOnLogin -bool true
 defaults write com.knollsoft.Rectangle hideMenubarIcon -bool true
 defaults write com.knollsoft.Rectangle windowSnapping -int 2
 defaults write com.knollsoft.Rectangle subsequentExecutionMode -int 1
+
+echo "You need to restart Rectangle app to apply changes."
